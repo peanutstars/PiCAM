7cb44564ca9b2817fcd7a6947b1b0bbff41536b9
peanutstars <peanutstars.lee@gmail.com>
2016-05-26 23:51:26 +0900

    piot, make packet layer to send/receive protocol.

--- src/utils/piot/lpiot/ipcpacket.py
+++ src/utils/piot/lpiot/ipcpacket.py
@@ -1,32 +1,134 @@
 
+import os ;
+import threading ;
 from lpiot.sockuid import UIDDaemon, UIDClient ;
-from lpiot.sockpid import IPCDaemon, IPCClient ;
+from lpiot.sockipc import IPCDaemon, IPCClient ;
 from libps.psDebug import CliColor, DBG, ERR ;
 
 
 class PacketDaemon :
-    UID_ADDRESS = '/tmp/picam/socket_uid'
+    BASE_ADDRESS = '/tmp/picam'
+    UID_ADDRESS = BASE_ADDRESS + '/socket_uid'
     UID_PORT = None ;
-    UID_QUIT = 'uidquit' ;
-    IPC_ADDRESS = '/tmp/picam/socket_ipc'
+    UID_QUIT = 'uidquit\n' ;
+    IPC_ADDRESS = BASE_ADDRESS + '/socket_ipc'
     IPC_PORT = None ;
     IPC_QUIT = 'ipcquit' ;
     TERMINATOR = '\n' ;
-    def __init__(self) :
-        pass ;
+    def __init__(self, fgForce=False) :
+        if fgForce :
+            if not os.path.exists(PacketDaemon.BASE_ADDRESS) :
+                os.mkdir(PacketDaemon.BASE_ADDRESS) ;
+            if os.path.exists(PacketDaemon.UID_ADDRESS) :
+                os.remove(PacketDaemon.UID_ADDRESS) ;
+            if os.path.exists(PacketDaemon.IPC_ADDRESS) :
+                os.remove(PacketDaemon.IPC_ADDRESS) ;
     def start(self) :
-        UIDDaemon(UID_ADDRESS, UID_PORT) ;
-        IPCDaemon(IPC_ADDRESS, IPC_PORT) ;
+        UIDDaemon(PacketDaemon.UID_ADDRESS, PacketDaemon.UID_PORT) ;
+        IPCDaemon(PacketDaemon.IPC_ADDRESS, PacketDaemon.IPC_PORT) ;
     def stop(self) :
-        UIDClient(UID_ADDRESS, UID_PORT).getUID(UID_QUIT) ;
-        IPCClient(IPC_ADDRESS, IPC_PORT).sendMsg(PID_QUIT) ;
+        IPCClient(PacketDaemon.IPC_ADDRESS, PacketDaemon.IPC_PORT).sendMsg(PacketDaemon.IPC_QUIT) ;
+        UIDClient(PacketDaemon.UID_ADDRESS, PacketDaemon.UID_PORT).getUID(PacketDaemon.UID_QUIT) ;
+
+NOTIFY       = 'N' ;
+REQUEST      = 'R' ;
+REPLY        = 'r' ;
+SEPARATOR    = '|' ;
+SIZE_SUBTYPE = 6
 
 class PacketHandler :
-    def __init__(self) :
-        self.m_ipc = IPCClient(IPC_ADDRESS, IPC_PORT, self.receivedPacket) ;
+    # Packet Header :     XXXXXXXX(8)|Type(1)|SubType(6)|Payload
+    # NOTIFY_SENSOR_HEAD  00000000|N|SENSOR|
+    # REQUEST_DB_HEAD     12345678|R|DB    |
+    # REPLY_DB_HEAD       12345678|r|DB    |
+    def __init__(self, cbFunc=None) :
+        self.m_ipc       = IPCClient(PacketDaemon.IPC_ADDRESS, PacketDaemon.IPC_PORT, self.__receivedPacket) ;
+        # Callback Format : ID, SubType, Playload
+        self.m_callback  = cbFunc ;
+        self.m_queryPool = {} ;
+        self.m_lock = threading.Lock() ;
     def getUID(self) :
         return UIDClient(PacketDaemon.UID_ADDRESS, PacketDaemon.UID_PORT).getUID(PacketDaemon.TERMINATOR) ;
-    def receivedPacket(self, msg) :
-        pass ;
-    def sendPacket(self, msg) :
-        self.m_ipc.sendMsg(msg) ;
+    def __receivedPacket(self, msg) :
+        '''
+        receivedPacket() is called by IPCClient thread.
+        '''
+        field     = msg[0:18].split('|') ;
+        print '@@', field
+        if len(field) == 4 :
+            fieldId   = field[0] ;
+            fieldType = field[1] ;
+            fieldSub  = field[2] ;
+            if hasattr(self.m_callback, '__call__') :
+                payload = msg[18:] ;
+                if int(fieldId,16) != 0 :
+                    if fieldType == REPLY :
+                        with self.m_lock :
+                            try :
+                                self.m_queryPool[fieldId][1] = payload ;
+                                self.m_queryPool[fieldId][0].set() ;
+                            except KeyError :
+                                # Maybe, it is not mine.
+                                pass ;
+                    elif fieldType == REQUEST :
+                        self.m_callback(fieldId, fieldSub, payload) ;
+                else :
+                    self.m_callback(fieldId, fieldSub, payload) ;
+    def sendNotify(self, subType, payload) :
+        assert (len(subType) == SIZE_SUBTYPE), 'subType length is not 6.'
+        self.m_ipc.sendMsg('00000000'+SEPARATOR+NOTIFY+SEPARATOR + subType +SEPARATOR+ payload) ;
+    def sendQueryRequest(self, subType, payload, timeout=0) :
+        assert (len(subType) == SIZE_SUBTYPE), 'subType length is not 6.'
+        event = threading.Event() ;
+        fieldId = self.getUID() ;
+        self.m_ipc.sendMsg(fieldId+SEPARATOR+REQUEST+SEPARATOR + subType +SEPARATOR+ payload) ;
+        with self.m_lock :
+            self.m_queryPool[fieldId] = [event, None] ;
+        event.wait(None if timeout==0 else timeout) ;
+        with self.m_lock :
+            reply = self.m_queryPool.pop(fieldId) ;
+        if reply[1] == None :
+            DBG('Query Timeout[%d] - %s, %s' % (timeout, subType, payload))
+        return reply[1] ;
+    def sendQueryReply(self, fieldId, subType, payload) :
+        assert (len(subType) == SIZE_SUBTYPE), 'subType length is not 6.'
+        self.m_ipc.sendMsg(fieldId+SEPARATOR+REPLY+SEPARATOR + subType +SEPARATOR+ payload) ;
+
+if __name__ == '__main__':
+    import sys ;
+    class Handler :
+        def __init__(self, fgDoReply=False) :
+            self.m_handle = PacketHandler(self.onReceived) ;
+            self.m_fgDoReply = fgDoReply ;
+        def onReceived(self, packetId, subType, payload) :
+            if int(packetId,16) == 0 :
+                print 'Notify [%s] %s' % (subType, payload) ;
+            elif int(packetId,16) != 0 and self.m_fgDoReply :
+                print 'Reply[%s:%s] %s' % (packetId, subType, payload) ;
+                self.m_handle.sendQueryReply(packetId, subType, 'Reply') ;
+        def doRequest(self, timeout=0) :
+            return self.m_handle.sendQueryRequest('SUBTYP', 'Request', timeout) ;
+        def doNotify(self) :
+            self.m_handle.sendNotify('subtyp', 'Notify') ;
+
+    if len(sys.argv) == 1 :
+        print 'usage : %s <notify|request|reply|quit> [<timeout> <server>]' ;
+        raise SystemExit ;
+    if 'server' in sys.argv :
+        PacketDaemon(True).start() ;
+    if sys.argv[1] == 'quit' :
+        PacketDaemon(False).stop() ;
+        raise SystemExit ;
+
+    h = Handler(True if sys.argv[1] == 'reply' else False) ;
+    if sys.argv[1] == 'request' :
+        try :
+            timeout = int(sys.argv[2]) ;
+        except ValueError :
+            timeout = 0 ;
+        except IndexError :
+            timeout = 0 ;
+        rv = h.doRequest(timeout) ;
+        DBG('Replied : %s' % rv) ;
+    if sys.argv[1] == 'notify' :
+        h.doNotify() ;
--- src/utils/piot/lpiot/sockuid.py
+++ src/utils/piot/lpiot/sockuid.py
@@ -1,57 +1,70 @@
 
-import asyncore ;
+# import asyncore ;
 import socket ;
+import select ;
+import time ;
 import threading ;
 from libps.psDebug import CliColor, DBG, ERR ;
 
 UID_QUIT = 'uidquit'
 UID_TOKEN = '\n'
 
-uidCount = 0 ;
-uidLock = threading.Lock() ;
-
-class UIDServer(asyncore.dispatcher) :
-    def __init__(self, host, port) :
-        asyncore.dispatcher.__init__(self) ;
-        if isinstance(port, int) :
-            self.create_socket(socket.AF_INET, socket.SOCK_STREAM) ;
-            self.bind((host, port)) ;
-        else :
-            self.create_socket(socket.AF_UNIX, socket.SOCK_STREAM) ;
-            self.bind(host) ;
-        self.listen(1) ;
-    def handle_accept(self) :
-        sock, address = self.accept() ;
-        UIDHandler(sock) ;
-
-class UIDHandler(asyncore.dispatcher_with_send) :
-    def handle_read(self) :
-        buffer = self.recv(16) ;
-        if buffer == UID_TOKEN :
-            self.out_buffer = self.getUID() ;
-        elif buffer == UID_QUIT :
-            raise asyncore.ExitNow('UID Server is quitting') ;
-        if not self.out_buffer :
-            self.close() ;
-    def getUID(self) :
-        global uidCount ;
-        with uidLock :
-            uidCount += 1 ;
-            uidCount &= 0xFFFFFFFF ;
-            if uidCount == 0 :
-                uidCount = 1 ;
-            return '%08X' % uidCount ;
+# uidCount = 0 ;
+# uidLock = threading.Lock() ;
 
 class UIDDaemon(threading.Thread) :
+    CONNECT_TIMEOUT = 5 ;
     def __init__(self, host, port) :
         threading.Thread.__init__(self) ;
-        self.server = UIDServer(host, port) ;
+        if isinstance(port, int) :
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) ;
+            self.sock.bind((host, port)) ;
+        else :
+            self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) ;
+            self.sock.bind(host) ;
+        self.sock.listen(1) ;
+        self.uidCount = 0 ;
+        self.fgRun = True ;
         self.start() ;
+    def getUID(self) :
+        self.uidCount += 1 ;
+        self.uidCount &= 0xFFFFFFFF ;
+        if self.uidCount == 0 :
+            self.uidCount = 1 ;
+        return '%08X' % self.uidCount ;
     def run(self) :
-        try :
-            asyncore.loop() ;
-        except asyncore.ExitNow, e :
-            DBG(e) ;
+        while self.fgRun :
+            conn, address = self.sock.accept() ;
+
+            startTime = time.time() ;
+            poll = select.poll() ;
+            poll.register(conn, select.POLLIN | select.POLLHUP) ;
+            pollc = 1 ;
+            try :
+                buffer = [] ;
+                fgPollUnregister = False ;
+                while pollc > 0 and (time.time() - startTime) < UIDDaemon.CONNECT_TIMEOUT :
+                    events = poll.poll(1) ;
+                    for event in events :
+                        (rfd, event) = event ;
+                        if event & select.POLLIN :
+                            if rfd == conn.fileno() :
+                                data = conn.recv(1) ;
+                                if data != UID_TOKEN :
+                                    buffer.append(data) ;
+                                    continue ;
+                                else :
+                                    if len(buffer) == 0 :
+                                        conn.sendall(self.getUID()) ;
+                                    elif ''.join(buffer) == UID_QUIT :
+                                        self.fgRun = False ;
+                                    fgPollUnregister = True ;
+                        if fgPollUnregister or event & select.POLLHUP :
+                            poll.unregister(rfd) ;
+                            pollc -= 1 ;
+            finally :
+                conn.close() ;
+        self.sock.close() ;
 
 class UIDClient :
     def __init__(self, host, port) :
@@ -70,16 +83,79 @@ class UIDClient :
         return uid ;
 
 
+# class UIDHandler(asyncore.dispatcher_with_send) :
+#     def handle_read(self) :
+#         buffer = self.recv(16) ;
+#         if buffer == UID_TOKEN :
+#             self.out_buffer = self.getUID() ;
+#         elif buffer == UID_QUIT :
+#             raise asyncore.ExitNow('UID Server is quitting') ;
+#         if not self.out_buffer :
+#             self.close() ;
+#     def getUID(self) :
+#         global uidCount ;
+#         with uidLock :
+#             uidCount += 1 ;
+#             uidCount &= 0xFFFFFFFF ;
+#             if uidCount == 0 :
+#                 uidCount = 1 ;
+#             return '%08X' % uidCount ;
+#
+# class UIDServer(asyncore.dispatcher) :
+#     def __init__(self, host, port) :
+#         asyncore.dispatcher.__init__(self) ;
+#         if isinstance(port, int) :
+#             self.create_socket(socket.AF_INET, socket.SOCK_STREAM) ;
+#             self.bind((host, port)) ;
+#         else :
+#             self.create_socket(socket.AF_UNIX, socket.SOCK_STREAM) ;
+#             self.bind(host) ;
+#         self.listen(1) ;
+#     def handle_accept(self) :
+#         try :
+#             sock, address = self.accept() ;
+#             UIDHandler(sock) ;
+#         except TypeError, e :
+#             pass ;
+#
+# class UIDDaemon(threading.Thread) :
+#     def __init__(self, host, port) :
+#         threading.Thread.__init__(self) ;
+#         self.server = UIDServer(host, port) ;
+#         self.start() ;
+#     def run(self) :
+#         try :
+#             asyncore.loop() ;
+#         except asyncore.ExitNow, e :
+#             DBG(e) ;
+#
+# class UIDClient :
+#     def __init__(self, host, port) :
+#         if isinstance(port, int) :
+#             self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) ;
+#             self.sock.connect((host,port)) ;
+#         else :
+#             self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) ;
+#             self.sock.connect(host) ;
+#     def getUID(self, msg=UID_TOKEN) :
+#         try :
+#             self.sock.sendall(msg) ;
+#             uid = self.sock.recv(16) ;
+#         finally :
+#             self.sock.close() ;
+#         return uid ;
+#
+#
 if __name__ == '__main__':
     import sys ;
-    uidHost = 'uidport' ;
+    uidHost = '/tmp/picam/socket_uid' ;
     uidPort = None ;
 
     def main(argv) :
         if argv[0] == 'server' :
             UIDDaemon(uidHost, uidPort) ;
         elif argv[0] == 'client' :
-            DBG(UIDClient(uidHost, uidPort).getUID('\n' if len(argv) == 1 else argv[1])) ;
+            DBG(UIDClient(uidHost, uidPort).getUID('\n' if len(argv) == 1 else argv[1]+'\n')) ;
 
     if len(sys.argv) == 1 :
         DBG('usage : %s <server|client> [message]' % sys.argv[0]) ;
